f113e00a938e01ff4bba1be567b4ed978b617297
nbruno0809 <nbruno0809@outlook.com>
2023-05-12 19:57:13 +0200

    KillTests

---
+++ Virologists/src/test/java/iet/KillTests.java
@@ -0,0 +1,68 @@
+package test.java.iet;
+
+import main.java.iet.Agents.BearAgent;
+import main.java.iet.Core.Game;
+import main.java.iet.Core.Virologist;
+import main.java.iet.Equipments.Axe;
+import main.java.iet.Fields.Field;
+import main.java.iet.KillBehaviours.CantKill;
+import main.java.iet.KillBehaviours.KillWithAxe;
+import main.java.iet.MoveBehaviours.BearMove;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KillTests {
+    private Game game;
+    private Field field;
+    private Virologist attacker;
+    private Virologist victim;
+
+    @BeforeEach
+    public void onTimeSetup() {
+        game = new Game(2);
+        field = new Field();
+        attacker = new Virologist(game,field);
+        victim = new Virologist(game,field);
+    }
+
+    @Test
+    public void KillBearWithAxeTest() {
+        var axe = new Axe(attacker);;
+        attacker.getEquipments().add(axe);
+        attacker.setKillBehaviour(new KillWithAxe(axe));
+
+        victim.setMoveBehaviour(new BearMove());
+        attacker.UseKill(victim);
+
+        assertFalse(field.getVirologists().contains(victim));
+        assertFalse(game.getVirologists().contains(victim));
+        assertEquals(0,axe.getNumberOfUse());
+        assertEquals(CantKill.class,attacker.getKillBehaviour().getClass());
+        //assertFalse(attacker.getEquipments().contains(axe));
+
+
+    }
+
+    @Test
+    public void KillNormalPlayerWithAxe() {
+        var axe = new Axe(attacker);;
+        attacker.getEquipments().add(axe);
+        attacker.setKillBehaviour(new KillWithAxe(axe));
+        attacker.UseKill(victim);
+
+        assertTrue(field.getVirologists().contains(victim));
+        assertTrue(game.getVirologists().contains(victim));
+        assertEquals(1,axe.getNumberOfUse());
+    }
+
+    @Test
+    public void KillWhenCantKillTest() {
+        attacker.setKillBehaviour(new CantKill());
+        attacker.UseKill(victim);
+
+        assertTrue(field.getVirologists().contains(victim));
+        assertTrue(game.getVirologists().contains(victim));
+    }
+}
